f380d0be07e45136aae27773c0a4f141d78ce256
Glauber Henrique <glauberhenrrique2024@gmail.com>
2026-02-02 00:21:33 -0300

    Project created

---
+++ src/test/java/g/sena/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package g.sena.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
